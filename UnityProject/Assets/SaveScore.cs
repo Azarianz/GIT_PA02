d9cc884414915a6748e91ab1a3b72636cd03f829
Azarianz <62070676+Azarianz@users.noreply.github.com>
2020-06-22 12:04:18 +0800

    Final Update before Submission
    Unity Project, Exe, Word Document

---
+++ UnityProject/Assets/SaveScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SaveScore : MonoBehaviour
+{
+    public TextMeshProUGUI HighScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("SavedScore", GameManager.Score);
+        HighScoreText.text = "Score: " + PlayerPrefs.GetInt("SavedScore");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/Assets/_UI/HUD.cs
@@ -0,0 +1,46 @@
+﻿﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public static HUD HUDManager;
+    [SerializeField] private Text Txt_Score = null;
+    [SerializeField] private Image Image_Lives = null;
+    [SerializeField] private Text Txt_Message = null;
+ 
+    void Start()
+    {
+        HUDManager = this;
+    }
+
+    public void UpdateScore()
+    {
+        Txt_Score.text = "SCORE : " + GameManager.Score;
+    }
+
+    //updates the number of hearts for lives
+    public void UpdateLives()
+    {
+        Image_Lives.rectTransform.sizeDelta = new Vector2(GameManager.Lives * 50, 30);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        GameManager.CurrentState = GameManager.GameState.GameOver;
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void DismissMessage()
+    {
+        Txt_Message.text = "";
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
